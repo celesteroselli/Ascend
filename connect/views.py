f7e9f15c81f0bafb5081dd44a2b9ce035338004a
Celeste Roselli <72556836+celesteroselli@users.noreply.github.com>
2024-11-16 22:44:34 -0500

    Added photos to camera roll, changed bgs, tested, ready!!

--- connect/views.py
+++ connect/views.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from django.views.generic.edit import CreateView
 
 def home(request):
-    list1 = [1, 2, 3, 4, 5]
-    dates1 = ["10/3/22", "3/14/19", "5/12/17", "9/17/23", "12/20/13"]
+    list1 = [1, 2, 3, 4, 5, 6]
+    dates1 = ["11/15/24", "11/15/24", "11/16/24", "11/16/24", "11/17/24", "11/17/24"]
     randomnum = (random.choice(list1))
     date = dates1[randomnum-1]
     print(randomnum)
-    image = os.path.join(BASE_DIR,str("media/images/"+str(randomnum))+".jpg/")
+    image = os.path.join(BASE_DIR,str("media/images/"+str(randomnum))+".JPEG/")
     print(image)
     bg = request.user.profile.image
 
@@ -34,7 +34,7 @@ def home(request):
             print(form.errors)
 
     connections = Connection.objects.filter(astronaut=request.user)
-    return render(request, "picture.html", {"connections":connections, "image_url":image, "date":date, "bg":bg, "form":form})
+    return render(request, "picture.html", {"connections":connections, "image_url":image, "date":date, "bg":bg, "cform":form})
 
 def connection(request, username):
     form = MessageForm(request.POST or None)
@@ -51,8 +51,25 @@ def connection(request, username):
         else:
             print("invalid")
             print(form.errors)
+    bg = request.user.profile.image
+
+    form2 = ChangeImage(instance=Profile.objects.get(user=request.user))
+    if request.method == 'POST':
+        form2 = ChangeImage(request.POST,request.FILES, instance=Profile.objects.get(user=request.user))
+        if form2.is_valid():
+            change = form2.save()
+            request.user.profile.image = change.image
+            change.save()
+            print("valid")
+            print(request.user.profile.image)
+            return redirect('home')
+        else:
+            print("invalid")
+            print(form2.errors)
+
+
     earthling=User.objects.get(username=username)
     message1 = Message.objects.filter(Q(msg_to=earthling)|Q(msg_from=earthling))
     message2 = Message.objects.filter(Q(msg_to=request.user)|Q(msg_from=request.user))
     messages = message1.intersection(message2).order_by('-id')
-    return render(request, "videoview.html", {"messages":messages, "form": form})
+    return render(request, "videoview.html", {"messages":messages, "form": form, "bg":bg, "cform": form2})
